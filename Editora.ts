7f653855ac419bbe781595af6532aab78470986a
altairsf <altair.regina.26@gmail.com>
2024-11-25 00:51:09 -0300

    Add files via upload

--- Editora.ts
+++ Editora.ts
@@ -1,4 +1,9 @@
-class Editora{
+export class Editora {
     codEditora: number;
     nome: string;
+
+    public constructor(codEditora: number, nome: string) {
+        this.codEditora = codEditora;
+        this.nome = nome;
+    }
 }
--- Livro.ts
+++ Livro.ts
@@ -1,7 +1,15 @@
-class Livro{
+export class Livro {
     codigo: number;
     codEditora: number;
     título: string;
     resumo: string;
-    autores: stringArr;
+    autores: string[];
+
+    constructor(codigo: number, codEditora: number, título: string, resumo: string, autores: string[]) {
+        this.codigo = codigo;
+        this.codEditora = codEditora;
+        this.título = título;
+        this.resumo = resumo;
+        this.autores = autores;
+    }
 }
